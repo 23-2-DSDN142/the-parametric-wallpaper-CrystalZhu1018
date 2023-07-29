66805d51553e636c52d8ca90d4b3e34ba02cc3df
Crystal Zhu <mia20011018@icloud.com>
2023-07-30 04:08:18 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -22,42 +22,88 @@ function wallpaper_background() {
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 var x = 0;
 var y = 200;
-var z = 80;//middle points of first one
-var a = 120;//middle points of second one
-var b = 50;//middle points of third one
-var c = 160;//middle points of fourth one
-var d = 20;//middle points of fifth one
+var n = 200;//left point height
+var o = 200;//right point height
+var z = 100;//middle points of first one
+var a = 100;//middle points of second one 
+var b = 100;//middle points of third one
+var c = 100;//middle points of fourth one
+var d = 100;//middle points of fifth one
+
+var e = 0;
+var f = 200;
+var l = 0;//left point height
+var m = 0;//right point height
+var g = 100;//middle points of first one
+var h = 100;//middle points of second one
+var i = 100; //middle points of third one
+var j = 100;//middle points of fourth one
+var k = 100;//middle points of fifth one
 
 fill(51);//noFill
 noStroke();
 beginShape();
 vertex(z, 0);//top point
-vertex(x, y);//left point
+vertex(x, n);//left point
 vertex(z, 20);//middle point
-vertex(y, y);//right point
+vertex(y, o);//right point
 endShape(CLOSE);
 
 vertex(a, 40);//top point
-vertex(x, y);//left point
+vertex(x, n);//left point
 vertex(a, 60);//middle point
-vertex(y, y);//right point
+vertex(y, o);//right point
 endShape(CLOSE);
 
 vertex(b, 80);//top point
-vertex(x, y);//left point
+vertex(x, n);//left point
 vertex(b, 100);//middle point
-vertex(y, y);//right point
+vertex(y, o);//right point
 endShape(CLOSE);
 
 vertex(c, 120);//top point
-vertex(x, y);//left point
+vertex(x, n);//left point
 vertex(c, 140);//middle point
-vertex(y, y);//right point
+vertex(y, o);//right point
 endShape(CLOSE);
 
 vertex(d, 160);//top point
-vertex(x, y);//left point
+vertex(x, n);//left point
 vertex(d, 180);//middle point
-vertex(y, y);//right point
+vertex(y, o);//right point
 endShape(CLOSE);
-}
+  
+  fill('red');
+  noStroke();
+  beginShape();
+  vertex(e, l);//left point
+  vertex(g, 180);//middle point
+  vertex(f, m);//right point
+  vertex(g, 200);//top point
+  endShape(CLOSE);
+  
+  vertex(e, l);//left point
+  vertex(h, 140);//middle point
+  vertex(f, m);//right point
+  vertex(h, 160);//top point
+  endShape(CLOSE);
+  
+  vertex(e, l);//left point
+  vertex(i, 100);//middle point
+  vertex(f, m);//right point
+  vertex(i, 120);//top point
+  endShape(CLOSE);
+  
+  vertex(e, l);//left point
+  vertex(j, 60);//middle point
+  vertex(f, m);//right point
+  vertex(j, 80);//top point
+  endShape(CLOSE);
+  
+  vertex(e, l);//left point
+  vertex(k, 20);//middle point
+  vertex(f, m);//right point
+  vertex(k, 40);//top point
+  endShape(CLOSE);
+
+  }
