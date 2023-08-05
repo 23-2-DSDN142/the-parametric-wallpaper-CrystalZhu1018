d9bed4bb81e3832258c43cbb42245e5b90e7f19a
Crystal Zhu <mia20011018@icloud.com>
2023-08-06 00:22:45 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,29 +1,51 @@
-//your parameter variables go here!
-let onepatternmode = true;
+//parameter variables
+let onepatternmode = false;
+//normal one
+var n = 200;//left point height
+var o = 0;//right point height
+var z = 100;//first middle points x
+let p1 = 80;//first middle points y
+let p2 = p1+5;//first middle points y
+var a = 100;//second middle points x
+let q1 = p1+15;//second middle points y
+let q2 = p1+20;//second middle points y
+var b = 100;//third middle points x
+let r1 = p1+30;//third middle points y
+let r2 = p1+35;//third middle points y
+var c = 100;//fourth middle points x
+let s1 = p1+45;//fourth middle points y
+let s2 = p1+50;//fourth middle points y
+var d = 100;//fifth middle points x
+let t1 = p1+60;//fifth middle points y
+let t2 = p1+65;//fifth middle points y
+//inverted one
+var l = 0;//left point height
+var m = 200;//right point height
+var g = 100;//first middle points x
+let u1 = 40;//first middle points y
+let u2 = 45;//first middle points y
+var h = 100;//second middle points x
+let v1 = 55;//second middle points y
+let v2 = 60;//second middle points y
+var i = 100; //third middle points x
+let w1 = 70;//third middle points y
+let w2 = 75;//third middle points y
+var j = 100;//fourth middle points x
+let x1 = 85;//fourth middle points y
+let x2 = 90;//fourth middle points y
+var k = 100;//fifth middle points x
+let y1 = 100;//fifth middle points y
+let y2 = 105;//fifth middle points y
+//variables that do not change
 var x = 0;
 var y = 200;
-var n = 200;//left point height
-var o = 200;//right point height
-var z = 100;//middle points of first one
-var a = 100;//middle points of second one 
-var b = 100;//middle points of third one
-var c = 100;//middle points of fourth one
-var d = 100;//middle points of fifth one
-
 var e = 0;
 var f = 200;
-var l = 0;//left point height
-var m = 0;//right point height
-var g = 100;//middle points of first one
-var h = 100;//middle points of second one
-var i = 100; //middle points of third one
-var j = 100;//middle points of fourth one
-var k = 100;//middle points of fifth one
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.output_mode(GLIDE_WALLPAPER); //GLIDE_WALLPAPER GRID_WALLPAPER
+  pWallpaper.resolution(NINE_PORTRAIT);//NINE_LANDSCAPE FIT_TO_SCREEN NINE_PORTRAIT
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -48,125 +70,125 @@ let LIGHTPINK = color(255, 161, 217);
 fill(BRIGHTPINK);//noFill
 noStroke();
 beginShape();
-vertex(z, 0);//top point
+vertex(z, p1);//top point
 vertex(x, n);//left point
-vertex(z, 20);//middle point
+vertex(z, p2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
 fill(MILDPINK);
 beginShape();
-vertex(a, 40);//top point
+vertex(a, q1);//top point
 vertex(x, n);//left point
-vertex(a, 60);//middle point
+vertex(a, q2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
 fill(MEDIUMPINK);
 beginShape();
-vertex(b, 80);//top point
+vertex(b, r1);//top point
 vertex(x, n);//left point
-vertex(b, 100);//middle point
+vertex(b, r2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
 fill(PINK);
 beginShape();
-vertex(c, 120);//top point
+vertex(c, s1);//top point
 vertex(x, n);//left point
-vertex(c, 140);//middle point
+vertex(c, s2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
 fill(LIGHTPINK);
 beginShape();
-vertex(d, 160);//top point
+vertex(d, t1);//top point
 vertex(x, n);//left point
-vertex(d, 180);//middle point
+vertex(d, t2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
 }else{
-fill(51);//noFill
+fill(0, 0, 0);//noFill
 noStroke();
 beginShape();
-vertex(z, 0);//top point
+vertex(z, p1);//top point
 vertex(x, n);//left point
-vertex(z, 20);//middle point
+vertex(z, p2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
-fill('red');
+fill(255, 0, 0, 180);
 beginShape();
-vertex(a, 40);//top point
+vertex(a, q1);//top point
 vertex(x, n);//left point
-vertex(a, 60);//middle point
+vertex(a, q2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
-fill(51); 
+fill(0, 0, 0); 
 beginShape();
-vertex(b, 80);//top point
+vertex(b, r1);//top point
 vertex(x, n);//left point
-vertex(b, 100);//middle point
+vertex(b, r2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
-fill('red');
+fill(255, 0, 0, 140);
 beginShape();
-vertex(c, 120);//top point
+vertex(c, s1);//top point
 vertex(x, n);//left point
-vertex(c, 140);//middle point
+vertex(c, s2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
-fill(51); 
+fill(0, 0, 0); 
 beginShape();
-vertex(d, 160);//top point
+vertex(d, t1);//top point
 vertex(x, n);//left point
-vertex(d, 180);//middle point
+vertex(d, t2);//middle point
 vertex(y, o);//right point
 endShape(CLOSE);
 
-  fill('red');
+  fill(255, 0, 0, 120);
   noStroke();
   beginShape();
   vertex(e, l);//left point
-  vertex(g, 180);//middle point
+  vertex(g, u1);//middle point
   vertex(f, m);//right point
-  vertex(g, 200);//top point
+  vertex(g, u2);//top point
   endShape(CLOSE);
   
-  fill(51); 
+  fill(0, 0, 0); 
   beginShape();
   vertex(e, l);//left point
-  vertex(h, 140);//middle point
+  vertex(h, v1);//middle point
   vertex(f, m);//right point
-  vertex(h, 160);//top point
+  vertex(h, v2);//top point
   endShape(CLOSE);
 
-  fill('red');
+  fill(255, 0, 0, 140);
   beginShape();
   vertex(e, l);//left point
-  vertex(i, 100);//middle point
+  vertex(i, w1);//middle point
   vertex(f, m);//right point
-  vertex(i, 120);//top point
+  vertex(i, w2);//top point
   endShape(CLOSE);
   
-  fill(51); 
+  fill(0, 0, 0); 
   beginShape();
   vertex(e, l);//left point
-  vertex(j, 60);//middle point
+  vertex(j, x1);//middle point
   vertex(f, m);//right point
-  vertex(j, 80);//top point
+  vertex(j, x2);//top point
   endShape(CLOSE);
   
-  fill('red');
+  fill(255, 0, 0, 160);
   beginShape();
   vertex(e, l);//left point
-  vertex(k, 20);//middle point
+  vertex(k, y1);//middle point
   vertex(f, m);//right point
-  vertex(k, 40);//top point
+  vertex(k, y2);//top point
   endShape(CLOSE);
 }
 
