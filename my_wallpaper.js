b9f8734bc233860dbb879acf97b41623458c637d
Crystal Zhu <mia20011018@icloud.com>
2023-08-07 13:08:13 +1200

    final changes

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,41 +1,41 @@
 //parameter variables
 let onepatternmode = false;
 //normal one
-var n = 200;//left point height
-var o = 0;//right point height
-var z = 100;//first middle points x
-let p1 = 80;//first middle points y
-let p2 = p1+5;//first middle points y
-var a = 100;//second middle points x
-let q1 = p1+15;//second middle points y
-let q2 = p1+20;//second middle points y
-var b = 100;//third middle points x
-let r1 = p1+30;//third middle points y
-let r2 = p1+35;//third middle points y
-var c = 100;//fourth middle points x
-let s1 = p1+45;//fourth middle points y
-let s2 = p1+50;//fourth middle points y
-var d = 100;//fifth middle points x
-let t1 = p1+60;//fifth middle points y
-let t2 = p1+65;//fifth middle points y
+var n = 100;//left point height
+var o = 100;//right point height
+var z = 60;//first middle points x
+let p1 = 0;//first middle points y
+let p2 = p1+20;//first middle points y
+var a = 10;//second middle points x
+let q1 = p1+10;//second middle points y
+let q2 = p1+60;//second middle points y
+var b = 10;//third middle points x
+let r1 = p1+20;//third middle points y
+let r2 = p1+90;//third middle points y
+var c = 10;//fourth middle points x
+let s1 = p1+50;//fourth middle points y
+let s2 = p1+160;//fourth middle points y
+var d = 10;//fifth middle points x
+let t1 = p1+190;//fifth middle points y
+let t2 = p1+200;//fifth middle points y
 //inverted one
-var l = 0;//left point height
-var m = 200;//right point height
+var l = 200;//left point height
+var m = 150;//right point height
 var g = 100;//first middle points x
-let u1 = 40;//first middle points y
-let u2 = 45;//first middle points y
+let u1 = 180;//first middle points y
+let u2 = 200;//first middle points y
 var h = 100;//second middle points x
-let v1 = 55;//second middle points y
-let v2 = 60;//second middle points y
+let v1 = 140;//second middle points y
+let v2 = 160;//second middle points y
 var i = 100; //third middle points x
-let w1 = 70;//third middle points y
-let w2 = 75;//third middle points y
+let w1 = 100;//third middle points y
+let w2 = 120;//third middle points y
 var j = 100;//fourth middle points x
-let x1 = 85;//fourth middle points y
-let x2 = 90;//fourth middle points y
+let x1 = 60;//fourth middle points y
+let x2 = 80;//fourth middle points y
 var k = 100;//fifth middle points x
-let y1 = 100;//fifth middle points y
-let y2 = 105;//fifth middle points y
+let y1 = 20;//fifth middle points y
+let y2 = 40;//fifth middle points y
 //variables that do not change
 var x = 0;
 var y = 200;
@@ -43,16 +43,15 @@ var e = 0;
 var f = 200;
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GLIDE_WALLPAPER); //GLIDE_WALLPAPER GRID_WALLPAPER
-  pWallpaper.resolution(NINE_PORTRAIT);//NINE_LANDSCAPE FIT_TO_SCREEN NINE_PORTRAIT
-  pWallpaper.show_guide(false); //set this to false when you're ready to print
+  pWallpaper.output_mode(GLIDE_WALLPAPER);
+  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.show_guide(false); 
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
   pWallpaper.grid_settings.row_offset  = 50;
 }
-
 function wallpaper_background() {
   background(0,0,0); //black colour
   background(255, 255, 255); //white colour
